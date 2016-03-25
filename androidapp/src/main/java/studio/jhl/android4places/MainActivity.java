4f9efd7d358417e53505b19cf5c0c1ea014e55a4
dmi3coder <dmi3coder@gmail.com>
2016-03-25 19:07:40 +0200

    REST api changes

--- androidapp/src/main/java/studio/jhl/android4places/MainActivity.java
+++ androidapp/src/main/java/studio/jhl/android4places/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
